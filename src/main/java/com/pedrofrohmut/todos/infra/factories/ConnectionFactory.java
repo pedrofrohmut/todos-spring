fc670c20f28a676251c2e421ef8701bf68a5c160
pedrofrohmut <pedrofrohmut@gmail.com>
2021-08-07 10:42:22 -0300

    DatabaseConfig

---
+++ src/main/java/com/pedrofrohmut/todos/infra/factories/ConnectionFactory.java
@@ -0,0 +1,23 @@
+package com.pedrofrohmut.todos.infra.factories;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import com.pedrofrohmut.todos.infra.errors.GetConnectionException;
+
+import static com.pedrofrohmut.todos.infra.config.DatabaseConfig.*;
+
+public class ConnectionFactory {
+
+  public Connection getConnection() {
+    try {
+      Class.forName(POSTGRES_DRIVER);
+      final var connection =
+        DriverManager.getConnection(POSTGRES_URL, POSTGRES_USER, POSTGRES_PASSWORD);
+      return connection;
+    } catch (Exception e) {
+      throw new GetConnectionException(e.getMessage());
+    }
+  }
+
+}
